85e85814edc4e6425d3590bf6af3d58e4130c71b
loultl <159482918+loultl@users.noreply.github.com>
2024-03-20 07:46:15 +0300

    ok

--- OPlab1/errorslib.h
+++ OPlab1/errorslib.h
@@ -4,8 +4,8 @@
 #include "appcontext.h"
 
 int checkDecSize(const char* input);
-int checkDecSize(const char* input);
-int checkDecSize(const char* input);
+int checkOctSize(const char* input);
+int checkBinSize(const char* input);
 
 void setErrorCode(AppContext* context, const char* input);
 void setError(AppContext* context);
