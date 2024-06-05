6d620abeab351af59b776e072c152e708e4aa082
pna23s042 <loultl@yandex.ru>
2024-06-05 18:55:24 +0300

    recursive, just like that

---
+++ OOPlab3/translator.h
@@ -0,0 +1,19 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
+#include <string>
+#include <vector>
+#include "token.h"
+#include "SyntaxError.h"
+
+class Translator
+{
+public:
+    Translator(std::string exp);
+    std::vector<Token> translate();
+private:
+    std::string expression;
+};
+
+
+#endif // TRANSLATOR_H
